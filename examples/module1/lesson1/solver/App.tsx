37e0cbd62750293c0ff7d26e0c3e2ebbe209e2a7
Paweł Sławuta <p.slawuta@limur.pl>
2024-06-18 21:21:05 +0200

    ref: lesson 1 solver

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import { f1, f2, f3, f4 } from './functions';
+import { useState } from 'react';
+import { add, divide, multiply, subtract } from './functions';
 
 const App = () => {
-  const [numA, setNumA] = useState<number>(0);
-  const [numB, setNumB] = useState<number>(0);
-  const [numC, setNumC] = useState<number | string>(0);
+  const [firstOperand, setFirstOperand] = useState<number>(0);
+  const [secondOperand, setSecondOperand] = useState<number>(0);
+  const [result, setResult] = useState<number | string>(0);
 
-  const doWork = (func: (a: number, b: number) => number) => {
-    setNumC(func(numA, numB));
+  const calculate = (calculationFunction: (a: number, b: number) => number) => {
+    setResult(calculationFunction(firstOperand, secondOperand));
+  };
+
+  const removeLeadingZeros = (value: string): string =>
+    value.replace(/^0+(?!$)/, '');
+
+  const getInputValue = (value: string): string => {
+    return removeLeadingZeros(value);
   };
 
   return (
@@ -16,43 +23,53 @@ const App = () => {
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numA}
-          onChange={(e) => setNumA(parseFloat(e.target.value))}
+          value={getInputValue(firstOperand.toString())}
+          onChange={(e) =>
+            setFirstOperand(parseFloat(removeLeadingZeros(e.target.value)))
+          }
         />
         <input
           type="number"
           className="rounded-md shadow-md p-4"
-          value={numB}
-          onChange={(e) => setNumB(parseFloat(e.target.value))}
+          value={getInputValue(secondOperand.toString())}
+          onChange={(e) =>
+            setSecondOperand(parseFloat(removeLeadingZeros(e.target.value)))
+          }
         />
       </div>
       <div className="grid grid-cols-4 gap-x-4 my-4">
         <button
           className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f1)}
+          onClick={() => calculate(add)}
         >
           +
         </button>
         <button
           className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f2)}
+          onClick={() => calculate(subtract)}
         >
           -
         </button>
         <button
           className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f3)}
+          onClick={() => calculate(multiply)}
         >
           *
         </button>
         <button
           className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f4)}
+          onClick={() => {
+            if (secondOperand === 0) {
+              setResult('Division by zero');
+              return;
+            }
+            calculate(divide);
+          }}
         >
           /
         </button>
       </div>
-      <div>Result: {numC}</div>
+      <div>Result: {result}</div>
     </div>
   );
 };
