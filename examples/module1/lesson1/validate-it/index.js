e1148d6948921d6857eb983db5f01c8a57688dc8
Paweł Sławuta <p.slawuta@limur.pl>
2024-06-18 20:50:46 +0200

    fix: lesson 1 validate-it

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -4,25 +4,29 @@ function validator() {
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
+  const renderInvalidMessage = () => {
+    result.innerHTML = 'Invalid';
+  };
+
   button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
+    const inputValue = Number(input.value);
+
+    if (!input.value) {
+      renderInvalidMessage();
+      return;
     }
+
+    if (!Number.isInteger(inputValue)) {
+      renderInvalidMessage();
+      return;
+    }
+
+    if (inputValue < 0 || inputValue > 100) {
+      renderInvalidMessage();
+      return;
+    }
+
+    result.innerHTML = 'Valid';
   });
 
   button2.addEventListener('click', () => {
