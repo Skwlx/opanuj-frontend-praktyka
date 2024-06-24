754701bfc0486a56cece603e4517d292681cde45
Paweł Sławuta <p.slawuta@limur.pl>
2024-06-24 15:43:34 +0200

    ref: change cart context to cart slice

--- examples/module1/lesson3/shop-rtk-start/state/cartSlice.ts
+++ examples/module1/lesson3/shop-rtk-start/state/cartSlice.ts
@@ -1,6 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { CartItem } from '../types/CartItem';
 import { RootState } from '../store';
+import { CartItem } from '../types/CartItem';
 import { Product } from '../types/Product';
 
 interface CartState {
@@ -31,15 +31,48 @@ export const cartSlice = createSlice({
         state.items.push(newItem);
       }
     },
+    decreaseAmount: (state, action: PayloadAction<{ id: number }>) => {
+      const cartItem = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (!cartItem) {
+        return;
+      }
+
+      if (cartItem.amount <= 1) {
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
+        return;
+      }
+
+      const newCart = state.items.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, amount: cartItem.amount - 1 }
+          : item
+      );
+      state.items = newCart;
+    },
+    removeFromCart: (state, action: PayloadAction<{ id: number }>) => {
+      state.items = state.items.filter((item) => {
+        return item.id !== action.payload.id;
+      });
+    },
     clearCart: (state) => {
       state.items = [];
     },
   },
 });
 
-export const { addToCart, clearCart } = cartSlice.actions;
+export const { addToCart, clearCart, decreaseAmount, removeFromCart } =
+  cartSlice.actions;
 
 export const selectCartItems = (state: RootState) => state.cart.items;
+export const selectTotalPrice = (state: RootState) =>
+  state.cart.items.reduce((total, item) => {
+    return total + item.price * item.amount;
+  }, 0);
 export const selectItemAmount = (state: RootState) =>
   state.cart.items.reduce((accumulator, currentItem) => {
     return accumulator + currentItem.amount;
